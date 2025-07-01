9c07e020790ec3edbc3071d17a58acb250e8b89f
rajiknows <rajeshhjhamain@gmail.com>
2025-07-01 19:54:11 +0530

    chore: lint fix

--- crankshaft-docker/src/bin/docker-driver.rs
+++ crankshaft-docker/src/bin/docker-driver.rs
@@ -137,7 +137,7 @@ async fn run(args: Args) -> Result<()> {
                 create_container(docker, image, tag, &name, command.remove(0), args).await?;
             let status = container.run(&name, None).await?;
 
-            println!("exit code: {}", status);
+            println!("exit code: {status}");
         }
         Command::RemoveContainer { name, force } => {
             let container = docker.container_from_name(name, None, None);

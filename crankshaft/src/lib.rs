7b9757f467ccd2e78d5d58425e76bf2c1b9379a3
Peter Huene <peter@huene.dev>
2025-08-26 11:55:13 -0400

    chore: fix formatting.

--- crankshaft/src/lib.rs
+++ crankshaft/src/lib.rs
@@ -6,6 +6,9 @@ pub use crankshaft_config as config;
 #[cfg(feature = "config")]
 #[doc(inline)]
 pub use crankshaft_config::Config;
+#[cfg(feature = "docker")]
+#[doc(inline)]
+pub use crankshaft_docker as docker;
 #[cfg(feature = "engine")]
 #[doc(inline)]
 pub use crankshaft_engine as engine;
@@ -15,6 +18,3 @@ pub use crankshaft_engine::Engine;
 #[cfg(feature = "events")]
 #[doc(inline)]
 pub use crankshaft_events as events;
-#[cfg(feature = "docker")]
-#[doc(inline)]
-pub use crankshaft_docker as docker;

17f3248fb7ee67cff77fdcfe1576e7cab7740283
Clay McLeod <clay.l.mcleod@gmail.com>
2026-09-25 15:48:17 -0500

    docs: add a red `$` prompt to the home page install command

--- docs/.vitepress/theme/components/CopyCommand.vue
+++ docs/.vitepress/theme/components/CopyCommand.vue
@@ -19,7 +19,7 @@ async function copy() {
 
 <template>
   <div class="copy-command" :data-status="status">
-    <code class="copy-command__text">{{ command }}</code>
+    <code class="copy-command__text"><span class="copy-command__prompt" aria-hidden="true">$</span>{{ command }}</code>
     <button type="button" class="copy-command__button" :aria-label="`Copy ${command}`" @click="copy">
       <svg v-if="status === 'copied'" viewBox="0 0 24 24" aria-hidden="true"><path d="M5 12.5l4.5 4.5L19 7.5" /></svg>
       <svg v-else viewBox="0 0 24 24" aria-hidden="true"><rect x="8" y="8" width="12" height="12" rx="1.5" /><path d="M16 8V5.5A1.5 1.5 0 0 0 14.5 4h-9A1.5 1.5 0 0 0 4 5.5v9A1.5 1.5 0 0 0 5.5 16H8" /></svg>
